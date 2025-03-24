7f37c569d55c66f4487f4edba09560eabd4750d1
Stefan Bobrowski <stefanbobrowski1@gmail.com>
2025-03-24 00:10:04 -0500

    remove ssl stuff for cloud sql private socket

--- backend/db.js
+++ backend/db.js
@@ -7,10 +7,6 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-
-  ssl: {
-    rejectUnauthorized: false, // disables certificate validation for dev
-  },
 });
 
 module.exports = pool;
