352fe934e1a69399c05ab3d3892d9f4edd22a40f
Stefan Bobrowski <stefanbobrowski1@gmail.com>
2025-03-23 23:58:54 -0500

    use cloud sql instance name

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,12 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  host: process.env.DB_HOST,
+  // host: process.env.DB_HOST,
+  host: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`, // 👈 this is the socket path
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+
   ssl: {
     rejectUnauthorized: false, // disables certificate validation for dev
   },
