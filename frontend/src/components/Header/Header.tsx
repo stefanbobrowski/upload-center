92b1d5bca76c43420af48fc3e2e45bb566e41f69
Stefan Bobrowski <stefanbobrowski1@gmail.com>
2025-04-29 03:50:33 -0500

    fix build

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,25 +1,7 @@
-// import { useState, useEffect } from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import cloudImg from '../../assets/cloud.png';
 import './header.scss';
 
 const Header = () => {
-  // const [darkMode, setDarkMode] = useState<boolean>(() => {
-  //   const stored = localStorage.getItem('theme');
-  //   if (stored) return stored === 'dark';
-  //   const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  //   return prefersDark;
-  // });
-
-  // useEffect(() => {
-  //   const theme = darkMode ? 'dark' : 'light';
-  //   document.documentElement.setAttribute('theme', theme);
-  //   localStorage.setItem('theme', theme);
-  // }, [darkMode]);
-
-  // const handleToggleDarkMode = () => {
-  //   setDarkMode((prev) => !prev);
-  // };
 
   return (
     <header>
@@ -45,20 +27,7 @@ const Header = () => {
             Examples
           </NavLink>
         </nav>
-        {/* <div className='theme-toggle'>
-          <span>Light</span>
-          <label className='switch'>
-            <input
-              type='checkbox'
-              onChange={handleToggleDarkMode}
-              checked={darkMode}
-            />
-            <span className='slider'></span>
-          </label>
-          <span>Dark</span>
-        </div> */}
         <div>
-
         </div>
       </div>
     </header>
