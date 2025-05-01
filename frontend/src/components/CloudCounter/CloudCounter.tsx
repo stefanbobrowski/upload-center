b6a82ca83951fdd0a8f2ef5af5025086e105ab20
Stefan Bobrowski <stefanbobrowski1@gmail.com>
2025-04-30 19:48:52 -0500

    fix build

--- frontend/src/components/CloudCounter/CloudCounter.tsx
+++ frontend/src/components/CloudCounter/CloudCounter.tsx
@@ -1,26 +1,15 @@
 import { useRequestCounter } from '../../context/RequestCounterContext';
 import { MAX_REQUESTS_PER_HOUR } from '../../constants/rateLimits';
-import cloudImg from '../../assets/cloud.png';
 import './cloud-counter.scss';
 
 const CloudCounter = () => {
   const { requestsRemaining } = useRequestCounter();
-
-  const counterColor =
-    requestsRemaining === 0
-      ? '#ff5c5c' // red
-      : requestsRemaining <= 3
-        ? '#ffb84d' // orange
-        : '#ffd93d'; // bright yellow
 
   const remainingText = requestsRemaining === null ? '?' : requestsRemaining;
 
   return (
     <div className="cloud-counter">
       <div className="cloud-container">
-        {/* <div className="cloud-image">
-          <img src={cloudImg}></img>
-        </div> */}
         <div className="rate-limit-counter">
           Requests: {remainingText}/{MAX_REQUESTS_PER_HOUR}
         </div>
